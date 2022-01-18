de69467973ba51c99ec0748a027f5ef922ac75e6
Alexander Batyrgariev <sasha@bashkort.org>
2022-01-19 04:07:55 +0500

    Added dist requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup
 
+requirements = [
+    "aiohttp>=3.7.4.post0,<4",
+    "websockets==9.1",
+    "pyhumps==1.6.1",
+    "web3==5.25.0",
+    "orjson==3.4.7"
+]
+
 setup(
     name='bloxroute',
     packages=['bloxroute_cli', 'bxgateway'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    install_requires=requirements
 )
